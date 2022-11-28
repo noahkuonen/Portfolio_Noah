5b7b00ebca4817666a7700d063ff928f50470423
noahkuonengit config --global user.name noahkuonengit config --global user.email noahkuonen@gmail.com <noahkuonen@gmail.com>
2022-11-28 16:41:46 +0100

    Letzte Änderungen

--- javascript.js
+++ javascript.js
@@ -1,23 +1,3 @@
-/*document.addEventListener('mousemove', function(e){
-    let body = document.querySelector('body');
-    let partikel = document.createElement('span');
-    let x = e.offsetX;
-    let y = e.offsetY;
-    partikel.style.left = x+'px';
-    partikel.style.top = y+'px';
-    
-    let size = Math.random()*8;
-    partikel.style.width = 4 + size+'px';
-    partikel.style.height = 4 + size+'px';
-
-
-    body.appendChild(partikel);
-
-    setTimeout(function(){
-        partikel.remove()
-    }, 2000)
-})*/
-
 /*Dark Mode Function*/
 
 function DarkMode() {
